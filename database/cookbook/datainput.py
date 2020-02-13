2ebc042a912f35af6c35f5cc7a252075949fe611
Elle Lang <langzx@uw.edu>
2020-02-12 18:57:11 -0800

    df

--- database/cookbook/datainput.py
+++ database/cookbook/datainput.py
@@ -11,3 +11,27 @@ state['SH'] = state['SH'].apply(lambda x: "'" + str(x) + "'")
 state['Date'] = state['Date'].apply(lambda x: "'" + str(x) + "'")
 state['Record'] = state['Record'].apply(lambda x: str(x) + ")")	
 state.to_csv(data_folder/'stateinput.txt',sep=',', index=False, header=False)
+
+
+tax = pd.read_csv(data_folder/"sales_tax_rate.txt",sep='\t',header = None)
+tax.head()
+tax.columns = ['State','Rate']
+tax['State'] = tax['State'].apply(lambda x: "('" + str(x) + "'")
+tax['Rate'] = tax['Rate'].apply(lambda x: str(x) + ")")	
+tax.to_csv(data_folder/'taxinput.txt',sep=',', index=False, header=False)
+
+
+city = pd.read_csv(data_folder/"city.txt",sep='\t',header = None)
+city.head()
+city.columns = ['State','Capital','Center']
+city['State'] = city['State'].apply(lambda x: "('" + str(x) + "'")
+city['Capital'] = city['Capital'].apply(lambda x: "'" + str(x) + "'")
+city['Center'] = city['Center'].apply(lambda x: "'" + str(x) + "')")
+city.to_csv(data_folder/'cityinput.txt',sep=',', index=False, header=False)
+
+winter = pd.read_csv(data_folder/"al_winner-2001.txt",sep='\t',header = None)
+winter.head()
+winter.columns = ['State','Degree']
+winter['State'] = winter['State'].apply(lambda x: "('" + str(x) + "'")
+winter['Degree'] = winter['Degree'].apply(lambda x: str(x) + ")")	
+winter.to_csv(data_folder/'winterinput.txt',sep=',', index=False, header=False)
