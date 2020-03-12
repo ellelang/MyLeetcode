c47385e5a4789bb6cfd5e2b81dbb86a507e56035
Elle Lang <langzx@uw.edu>
2020-03-12 11:18:01 -0700

    df

--- database/my1193.sql
+++ database/my1193.sql
@@ -1,3 +1,4 @@
+use leetcode;
 DROP TABLE IF EXISTS Transactions ;
 create table if not exists Transactions (id int, country varchar(4), state enum('approved', 'declined'), amount int, trans_date date);
 Truncate table Transactions;
@@ -5,6 +6,17 @@ insert into Transactions (id, country, state, amount, trans_date) values ('121',
 insert into Transactions (id, country, state, amount, trans_date) values ('122', 'US', 'declined', '2000', '2018-12-19');
 insert into Transactions (id, country, state, amount, trans_date) values ('123', 'US', 'approved', '2000', '2019-01-01');
 insert into Transactions (id, country, state, amount, trans_date) values ('124', 'DE', 'approved', '2000', '2019-01-07');
+
+
+select * 
+from transactions 
+sum (amount) over w as sum_
+window w as (partition by county order by left(trans_date, 7) );
+
+
+
+
+
 
 
 
