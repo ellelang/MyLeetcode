c9c257a47d07fb211917f157a22056d0dc1e1dc7
Elle Lang <langzx@uw.edu>
2020-04-26 08:49:13 -0700

    leetcode databse 1412,1421

--- database/my1412.sql
+++ database/my1412.sql
@@ -1,4 +1,6 @@
 USE LEETCODE;
+DROP TABLE IF exists Student;
+DROP TABLE IF exists Exam;
 Create table If Not Exists Student (student_id int, student_name varchar(30));
 Create table If Not Exists Exam (exam_id int, student_id int, score int);
 Truncate table Student;
@@ -18,3 +20,18 @@ insert into Exam (exam_id, student_id, score) values ('30', '4', '90');
 insert into Exam (exam_id, student_id, score) values ('40', '1', '60');
 insert into Exam (exam_id, student_id, score) values ('40', '2', '70');
 insert into Exam (exam_id, student_id, score) values ('40', '4', '80');
+
+With cte1 as(
+SELECT e.*,
+MAX(score) OVER (PARTITION BY exam_id) AS max_score,
+MIN(score) OVER (PARTITION BY exam_id) AS min_score
+FROM Exam e)
+
+SELECT student_id, student_name
+FROM Student
+WHERE student_id NOT IN(
+select student_id
+FROM cte1
+where score = max_score OR score = min_score) and 
+student_id IN (SELECT DISTINCT student_id FROM Exam);
+                     
